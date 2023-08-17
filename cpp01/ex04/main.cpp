2970ba80e08ef824f44d0a09b0fed7864f96c49e
flauer <flauer@student.42heilbronn.de>
2023-08-17 09:49:40 +0200

    using errno and checking input

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -6,18 +6,22 @@
 /*   By: flauer <flauer@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/15 14:19:31 by flauer            #+#    #+#             */
-/*   Updated: 2023/08/15 16:53:21 by flauer           ###   ########.fr       */
+/*   Updated: 2023/08/17 09:48:51 by flauer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <fstream>
 #include <iostream>
+#include <stdlib.h>
+#include <cstring>
 
-static bool	check_input(int &argc, char **&argv, char **&env)
+static bool	check_input(int &argc, char **&argv)
 {
-	(void)argc;
-	(void)argv;
-	(void)env;
+	if (argc != 4)
+		return (std::cerr << "Wrong number of Arguments!" << std::endl, false);
+	if (std::string(argv[2]).length() <= 0 ||
+		std::string(argv[3]).length() <= 0)
+		return (std::cerr << "s1 and s2 cannot be empty!" << std::endl,false);
 	return (true);
 }
 
@@ -36,24 +40,26 @@ static void	replace_in_line(std::string &line, std::string &s1, std::string &s2)
 	return ;
 }
 
-int	main(int argc, char **argv, char **env)
+int	main(int argc, char **argv)
 {
 	std::ifstream	input_file;
 	std::ofstream	output_file;
 	std::string		s1, s2, line, ofilename;
 
-	if (!check_input(argc, argv, env))
+	if (!check_input(argc, argv))
 		return (EXIT_FAILURE);
 	
 	input_file.open(argv[1], std::ifstream::in);
 	if (!input_file)
-		return (std::cout << "cannot open infile: " << argv[1] << std::endl, 1);
+		return (std::cerr << std::strerror(errno) << ": \"" << argv[1] << "\"" << std::endl,
+			EXIT_FAILURE);
 	ofilename = std::string(argv[1]) + ".replace";
 	output_file.open(ofilename.c_str(), std::ofstream::out | std::ofstream::trunc);
 	s1 = argv[2];
 	s2 = argv[3];
 	if (!output_file)
-		return (std::cout << "cannot open outfile: " << ofilename << std::endl, 1);
+		return (std::cerr << std::strerror(errno) << ": \"" << ofilename << "\"" << std::endl,
+			EXIT_FAILURE);
 	while (std::getline(input_file, line))
 	{
 		replace_in_line(line, s1, s2);
