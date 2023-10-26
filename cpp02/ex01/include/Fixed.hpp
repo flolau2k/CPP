c373f2d7d4849d67e7377001a458105385ddf4b7
flauer <flauer@student.42heilbronn.de>
2023-10-26 15:47:21 +0200

    ex01

--- cpp02/ex01/include/Fixed.hpp
+++ cpp02/ex01/include/Fixed.hpp
@@ -6,36 +6,45 @@
 /*   By: flauer <flauer@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/18 11:20:16 by flauer            #+#    #+#             */
-/*   Updated: 2023/08/18 16:19:26 by flauer           ###   ########.fr       */
+/*   Updated: 2023/10/26 13:36:46 by flauer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FIXED_HPP
-# define FIXED_HPP
+#define FIXED_HPP
 
-# include <iostream>
+#include <cmath>
+#include <iostream>
 
-# define COPYASSIGN "Copy assignment operator called"
-# define DESTRUCT "Destructor called"
-# define DEFCONST "Default constructor called"
-# define GETRAWC "getRawBits member function called"
-# define SETRAWC "setRawBits member function called"
-# define COPYCONST "Copy constructor called"
+#define COPYASSIGN "Copy assignment operator called"
+#define DESTRUCT "Destructor called"
+#define DEFCONST "Default constructor called"
+#define GETRAWC "getRawBits member function called"
+#define SETRAWC "setRawBits member function called"
+#define COPYCONST "Copy constructor called"
+#define INTCONST "Int constructor called"
+#define FLOATCONST "Float constructor called"
 
-class Fixed 
-{
-	public:
-		Fixed();
-		Fixed(const Fixed &F);
-		Fixed &operator=(const Fixed &F);
-		~Fixed();
+class Fixed {
+public:
+  Fixed();
+  Fixed(const int val);
+  Fixed(const float val);
+  Fixed(const Fixed &F);
+  ~Fixed();
 
-		int		getRawBits(void) const;
-		void	setRawBits(const int raw);
+  Fixed &operator=(const Fixed &F);
 
-	private:
-		int					_store;
-		static const int	fract = 8;
+  int getRawBits(void) const;
+  void setRawBits(const int raw);
+  float toFloat(void) const;
+  int toInt(void) const;
+
+private:
+  int _store;
+  static const int fract = 8;
 };
+
+std::ostream &operator<<(std::ostream &out, const Fixed &F);
 
 #endif
--- cpp02/ex01/src/Fixed.cpp
+++ cpp02/ex01/src/Fixed.cpp
@@ -1,16 +1,14 @@
-/* **************************************************************************
- */
+/* ************************************************************************** */
 /*                                                                            */
-/*                                                        :::      :::::::: */
-/*   Fixed.cpp                                          :+:      :+:    :+: */
-/*                                                    +:+ +:+         +:+ */
-/*   By: flauer <flauer@student.42heilbronn.de>     +#+  +:+       +#+ */
-/*                                                +#+#+#+#+#+   +#+ */
-/*   Created: 2023/08/18 11:20:21 by flauer            #+#    #+# */
-/*   Updated: 2023/10/26 10:15:15 by flauer           ###   ########.fr */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: flauer <flauer@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/26 13:36:36 by flauer            #+#    #+#             */
+/*   Updated: 2023/10/26 15:47:03 by flauer           ###   ########.fr       */
 /*                                                                            */
-/* **************************************************************************
- */
+/* ************************************************************************** */
 
 #include "../include/Fixed.hpp"
 
@@ -19,10 +17,19 @@ Fixed::Fixed() {
   _store = 0;
 }
 
+Fixed::Fixed(const int val) {
+  std::cout << INTCONST << std::endl;
+  _store = val << fract;
+}
+
+Fixed::Fixed(const float val) {
+  std::cout << FLOATCONST << std::endl;
+  _store = roundf(val * (1 << fract));
+}
+
 Fixed::Fixed(const Fixed &F) {
   std::cout << COPYCONST << std::endl;
-  *this = F; // this uses the Copy assignment operator
-  // this->_store = F.getRawBits(); // this does not.
+  *this = F;
 }
 
 Fixed::~Fixed() { std::cout << DESTRUCT << std::endl; }
@@ -43,4 +50,20 @@ int Fixed::getRawBits(void) const {
 void Fixed::setRawBits(const int raw) {
   std::cout << SETRAWC << std::endl;
   _store = raw;
+}
+
+/// @brief convert Fixed to float. Create a float from the _store integer and
+/// devide it by the number fractional bits.
+/// @param void
+/// @return floating point representation of Fixed
+float Fixed::toFloat(void) const {
+	return float(_store) / (1 << fract); 
+}
+
+int Fixed::toInt(void) const {
+	return roundf(this->toFloat()); 
+}
+
+std::ostream &operator<<(std::ostream &out, const Fixed &F) {
+  return out << F.toFloat();
 }
