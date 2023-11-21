4048cb26f725cae2f3801e3fb76a6f8e02e3d071
flauer <flauer@student.42heilbronn.de>
2023-11-21 11:02:00 +0100

    enh exit behaviour update gitignore

--- cpp00/ex01/src/main.cpp
+++ cpp00/ex01/src/main.cpp
@@ -6,19 +6,24 @@
 /*   By: flauer <flauer@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/11 15:24:06 by flauer            #+#    #+#             */
-/*   Updated: 2023/10/26 10:06:15 by flauer           ###   ########.fr       */
+/*   Updated: 2023/11/21 10:59:07 by flauer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "phonebook.hpp"
 
-static void print_help()
-{
-	std::cout << "Available Commands are: " << std::endl
-		<< "ADD: Adds a new contact to the list. " << std::endl
-		<< "SEARCH: List the contacts in the List." << std::endl
-		<< "EXIT: Quit the program, all data will be lost." << std::endl
-		<< "HELP: Display this help message." << std::endl;
+static void print_help() {
+  std::cout << "Available Commands are: " << std::endl
+            << "ADD: Adds a new contact to the list. " << std::endl
+            << "SEARCH: List the contacts in the List." << std::endl
+            << "EXIT: Quit the program, all data will be lost." << std::endl
+            << "HELP: Display this help message." << std::endl;
+}
+
+static void exit_phonebook(PhoneBook &pb) {
+  pb.~PhoneBook();
+  std::cout << "See Ya 🤙" << std::endl;
+  exit(0);
 }
 
 int main(void) {
@@ -27,17 +32,18 @@ int main(void) {
 
   std::cout << "Welcome to this wonderful PhoneBook!" << std::endl;
   while (true) {
-	if (std::cin.eof()) break;
     std::cout << "> ";
     std::getline(std::cin, buf);
+    if (std::cin.eof())
+      exit_phonebook(pb);
     if (buf == "ADD")
-		pb.AddContact();
-	else if (buf == "SEARCH")
-		pb.SearchContact();
-	else if (buf == "EXIT")
-		break;
-	else
-		print_help();
+      pb.AddContact();
+    else if (buf == "SEARCH")
+      pb.SearchContact();
+    else if (buf == "EXIT") {
+      exit_phonebook(pb);
+    } else
+      print_help();
   }
   return (0);
 }
