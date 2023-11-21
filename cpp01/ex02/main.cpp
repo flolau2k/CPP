3c9d2f2e7e9f49211ae1c79a08e6a177a24da626
flauer <flauer@student.42heilbronn.de>
2023-11-21 16:21:38 +0000

    change descriptions

--- cpp01/ex02/main.cpp
+++ cpp01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: flauer <flauer@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/14 17:12:50 by flauer            #+#    #+#             */
-/*   Updated: 2023/08/14 17:37:28 by flauer           ###   ########.fr       */
+/*   Updated: 2023/11/21 17:05:14 by flauer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,12 @@ int main(void)
 	std::string& stringREF = str;
 
 	std::cout << "Address of string: " << &str << std::endl
-		<< "Address of stringPTR: " << stringPTR << std::endl
-		<< "Address of stringREF: " << &stringREF << std::endl;
+		<< "Address held by stringPTR: " << stringPTR << std::endl
+		<< "Address held by stringREF: " << &stringREF << std::endl;
 
 	std::cout << "Value of string: " << str << std::endl
-		<< "Value of stringPTR: " << *stringPTR << std::endl
-		<< "Value of stringREF: " << stringREF << std::endl;
+		<< "Value pointed to by stringPTR: " << *stringPTR << std::endl
+		<< "Value pointed to by stringREF: " << stringREF << std::endl;
 
 	return (0);
 }
