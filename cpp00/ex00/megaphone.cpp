8e22c5b3032502fdeb035bc70a126b9a87a9525b
flauer <flauer@student.42heilbronn.de>
2023-11-21 14:26:19 +0100

    fix space printing

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: flauer <flauer@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/11 10:03:11 by flauer            #+#    #+#             */
-/*   Updated: 2023/08/11 13:47:05 by flauer           ###   ########.fr       */
+/*   Updated: 2023/11/21 14:24:08 by flauer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,20 @@ int main(int argc, char *argv[]) {
   }
   std::locale loc;
   std::string s;
+  bool space_printed = false;
   for (int i = 1; i < argc; i++) {
     s = argv[i];
     for (std::string::size_type j = 0; j < s.length(); j++) {
+		if (j == 0 && s[j] == ' ' && space_printed)
+			continue;
       std::cout << std::toupper(s[j], loc);
     }
-    std::cout << ' ';
+	space_printed = false;
+	if (argv[i][std::string(argv[i]).length() - 1] != ' ')
+	{
+		space_printed = true;
+    	std::cout << ' ';
+	}
   }
   std::cout << std::endl;
   return (0);
