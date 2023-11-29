36f933e23a6f9516b867584ff5ca8835df327ae0
flauer <flauer@student.42heilbronn.de>
2023-11-29 11:20:20 +0100

    format document

--- cpp02/ex00/Fixed.cpp
+++ cpp02/ex00/Fixed.cpp
@@ -6,48 +6,39 @@
 /*   By: flauer <flauer@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/18 11:20:21 by flauer            #+#    #+#             */
-/*   Updated: 2023/11/28 11:18:14 by flauer           ###   ########.fr       */
+/*   Updated: 2023/11/29 10:54:46 by flauer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 
-Fixed::Fixed() 
-{
-	std::cout << DEFCONST << std::endl;
-	_store = 0;
+Fixed::Fixed() {
+  std::cout << DEFCONST << std::endl;
+  _store = 0;
 }
 
-Fixed::Fixed(const Fixed &F)
-{
-	std::cout << COPYCONST << std::endl;
-	*this = F; // this uses the Copy assignment operator
-	// this->_store = F.getRawBits(); // this does not.
+Fixed::Fixed(const Fixed &F) {
+  std::cout << COPYCONST << std::endl;
+  *this = F; // this uses the Copy assignment operator
+  // this->_store = F.getRawBits(); // this does not.
 }
 
-Fixed::~Fixed()
-{
-	std::cout << DESTRUCT << std::endl;
+Fixed::~Fixed() { std::cout << DESTRUCT << std::endl; }
+
+Fixed &Fixed::operator=(const Fixed &F) {
+  std::cout << COPYASSIGN << std::endl;
+  if (this != &F) {
+    this->_store = F.getRawBits();
+  }
+  return *this;
 }
 
-Fixed &Fixed::operator=(const Fixed &F)
-{
-	std::cout << COPYASSIGN << std::endl;
-	if (this != &F)
-	{
-		this->_store = F.getRawBits();
-	}
-	return *this;
+int Fixed::getRawBits(void) const {
+  std::cout << GETRAWC << std::endl;
+  return _store;
 }
 
-int Fixed::getRawBits(void) const
-{
-	std::cout << GETRAWC << std::endl;
-	return _store;
-}
-
-void Fixed::setRawBits(const int raw)
-{
-	std::cout << SETRAWC << std::endl;
-	_store = raw;
+void Fixed::setRawBits(const int raw) {
+  std::cout << SETRAWC << std::endl;
+  _store = raw;
 }
