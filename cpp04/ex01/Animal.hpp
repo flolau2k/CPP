7dcb347afdb4eac5a27fe5d8ab1c5d0b7d47364b
flauer <flauer@student.42heilbronn.de>
2023-12-11 15:36:11 +0000

    add explanation

--- cpp04/ex01/Animal.hpp
+++ cpp04/ex01/Animal.hpp
@@ -1,15 +1,16 @@
 #ifndef ANIMAL_H
 #define ANIMAL_H
 
-#include <string>
 #include "defines.h"
+#include <string>
 
 class Animal {
 public:
   Animal();
   Animal(const Animal &other);
   Animal &operator=(const Animal &other);
-  virtual ~Animal();
+  virtual ~Animal(); // call the Destructor of Cat / Dog even if referred to as
+                     // an Animal *
 
   virtual void makeSound() const;
   const std::string &getType() const;
