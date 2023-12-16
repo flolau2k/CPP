52b376802c48369ee7db7f4fc558ffbfc966dafd
flolau2k <florian.lauer@live.de>
2023-12-16 17:43:17 +0100

    wip

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -1,2 +1,9 @@
+#include <iostream>
+#include <cstdlib>
 #include "ScalarConverter.hpp"
 
+void ScalarConverter::convert(const std::string &param) {
+  std::cout << "original string input: " << param << "\n";
+  std::cout << "as char: " << std::atoi(param.c_str()) << "\n";
+  std::cout << "as int: " << std::atoi(param.c_str());
+}
