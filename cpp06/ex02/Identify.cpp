5b3c3e9fcdaeeaa6cec8788f67d718ef185b7633
flauer <flauer@student.42heilbronn.de>
2024-01-12 11:22:08 +0100

    wip

--- cpp06/ex02/Identify.cpp
+++ cpp06/ex02/Identify.cpp
@@ -25,8 +25,6 @@ void identify(Base *p) {
     std::cout << "B!" << std::endl;
   else if (dynamic_cast<C*>(p) != NULL)
     std::cout << "C!" << std::endl;
-  else
-    std::cout << "bad!" << std::endl;
 }
 
 void identify(Base &p) {
