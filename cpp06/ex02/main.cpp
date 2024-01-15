1ceee594eb6510ca990297510c65f79795f77aa0
flauer <flauer@student.42heilbronn.de>
2024-01-15 16:47:47 +0100

    fix leak

--- cpp06/ex02/main.cpp
+++ cpp06/ex02/main.cpp
@@ -6,12 +6,11 @@
 int main(void) {
   std::srand(std::time(NULL));
   Base *ptr = generate();
-  while (ptr == NULL)
-    ptr = generate();
 
   std::cout << "identifying ptr..." << std::endl;
   identify(ptr);
   std::cout << "identifying ref..." << std::endl;
   identify(*ptr);
+  delete (ptr);
   return 0;
 }
