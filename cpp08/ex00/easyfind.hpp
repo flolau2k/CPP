0c6500ca48a9265e43c44b96ec2dcfd2d7d4586f
flauer <flauer@student.42heilbronn.de>
2024-01-15 14:23:36 +0100

    cpp08 wip

---
+++ cpp08/ex00/easyfind.hpp
@@ -0,0 +1,9 @@
+#ifndef EASYFIND_HPP
+#define EASYFIND_HPP
+
+template <typename T>
+int &easyfind(T &haystack, int needle) {
+  
+}
+
+#endif // EASYFIND_HPP
---
+++ cpp08/ex00/test.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <vector>
+
+int main(void) {
+  std::cout << "begin ..." << std::endl;
+  std::vector<int> vec;
+  
+}
