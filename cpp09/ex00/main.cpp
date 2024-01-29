04ac08965f644f2f07a898cda4b152ebe9ed0044
flolau2k <florian.lauer@live.de>
2024-01-29 17:44:17 +0100

    update main

--- cpp09/ex00/main.cpp
+++ cpp09/ex00/main.cpp
@@ -6,9 +6,28 @@
 #include <sstream>
 #include <string>
 
-int main(void) {
-  BitcoinExchange btc("data.csv");
-  const std::map<Date, double> data = btc.get_map();
+int main(int argc, char **argv) {
+  const std::string data_file = "test.csv";
+
+  if (argc != 2) {
+    std::cerr << "Wrong number of args; no input file given!" << std::endl;
+    return 1;
+  }
+
+  BitcoinExchange btc;
+
+  try {
+    btc.read_data(data_file);
+  } catch (std::exception &e) {
+    std::cerr << data_file << ": " << e.what() << std::endl;
+    return 1;
+  }
+  try {
+    btc.read_input(argv[1]);
+  } catch (std::exception &e) {
+    std::cerr << argv[1] << ": " << e.what() << std::endl;
+  }
+
 
   // typedef std::map<Date, double>::const_iterator iter;
   // for (iter it = data.begin(); it != data.end(); ++it) {
