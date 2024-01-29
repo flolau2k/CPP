4249919ba99e8fab76ec289f35faacae3f5a38c2
flolau2k <florian.lauer@live.de>
2024-01-29 19:38:32 +0100

    initial commit ex01

---
+++ cpp09/ex01/RPN.cpp
@@ -0,0 +1,9 @@
+#include "RPN.hpp"
+
+RPN::RPN() {}
+
+RPN::RPN(const RPN &cpy) {}
+
+RPN::~RPN() {}
+
+RPN &RPN::operator=(const RPN &other) {}
---
+++ cpp09/ex01/RPN.hpp
@@ -0,0 +1,15 @@
+#ifndef RPN_HPP
+#define RPN_HPP
+
+class RPN {
+public:
+  RPN();
+  RPN(const RPN &cpy);
+  RPN &operator=(const RPN &other);
+  ~RPN();
+
+private:
+
+};
+
+#endif // RPN_HPP
