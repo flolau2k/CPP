77c96a2098a9f953492ae5726e3b58e141a7dcbd
flauer <flauer@student.42heilbronn.de>
2024-01-30 16:10:13 +0100

    wip

---
+++ cpp09/ex02/main.cpp
@@ -0,0 +1,10 @@
+#include <sys/time.h>
+#include <iostream>
+
+int main(int argc, char **argv) {
+  if (argc < 2) {
+    std::cerr << "Error: No Args given!" << std::endl;
+    return 1;
+  }
+  
+}
