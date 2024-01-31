cde66c88c0586c4a23c76d690ec39c6afcbc1830
flauer <flauer@student.42heilbronn.de>
2024-01-31 15:24:12 +0100

    wip...

--- cpp09/ex02/PmergeMe.hpp
+++ cpp09/ex02/PmergeMe.hpp
@@ -1,6 +1,16 @@
 #ifndef PMERGEME_HPP
 #define PMERGEME_HPP
 
+class PmergeMe {
+public:
+  PmergeMe();
+  PmergeMe(const PmergeMe &cpy);
+  PmergeMe &operator=(const PmergeMe &other);
+  ~PmergeMe();
 
+private:
+  
+
+};
 
 #endif // PMERGEME_HPP
